388a80d02bb7af0500b35f3a3157dd60f6d4a5c2
zKevGs <kevinceriotti4@gmail.com>
2022-09-18 22:33:37 -0300

    agregue el segundo index y el segundo js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
- 
-alert('Hello world!')
+var pais = prompt("Ingrese un pais: ");
+var continente = prompt("Ingrese el continente: ");
+var antiguedad = prompt("Antigüedad: ");
+var pais_y_continente = pais + " de " + continente;
+
+pais = "Mexico";
+continente = "America";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
